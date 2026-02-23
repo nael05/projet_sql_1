c9190684e5cb7e41479faaa29acc41552aab1bf2
nael05 <nael.morellon@ynov.com>
2026-02-23 09:41:00 +0100

    requêtes Quête 1 – afficher et filtrer véhicules selon les conditions ville, disponibilité et autonomie

---
+++ quete_1.sql
@@ -0,0 +1,14 @@
+SELECT modele -- selection du modele
+FROM vehicule -- depuis la table vehicule
+
+SELECT modele -- selection du modele
+FROM vehicule -- depuis la table vehicule
+WHERE etat IS "Disponible" --condition de disponibilité
+
+SELECT modele -- selection du modele
+FROM vehicule -- depuis la table vehicule
+WHERE ville = "Paris" --condition de localisation
+
+SELECT modele -- selection du modele
+FROM vehicule -- depuis la table vehicule
+WHERE autonomie_km > 400 --condition d'autonomie
